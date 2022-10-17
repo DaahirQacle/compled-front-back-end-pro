e0e7a0e512811d59bd921078c72e50d94c36f084
DaahirQacle <daahirqacle@gmail.com>
2022-10-17 11:27:30 +0300

    updated

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
+import dotenv from 'dotenv'
 import express  from "express";
 import mongoose from "mongoose";
 import project from './Routes/projects.js'
 import cors from 'cors';
 const app = express();
+dotenv.config()
 app.use(cors())
 app.use(express.json())
-const port = 400
+const port =process.env.PORT || 400
 const URL = "mongodb+srv://qacle:7827@pro-managements.f368x9q.mongodb.net/?retryWrites=true&w=majority"
 
 app.use('/api' , project)
